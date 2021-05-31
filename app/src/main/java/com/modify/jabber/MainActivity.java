3a05dd41c763a61d8662f099da314d7e01f321b9
Jared Clapp <jtclapp10@gmail.com>
2021-05-31 17:44:59 -0500

    Firebase connected

--- app/src/main/java/com/modify/jabber/MainActivity.java
+++ app/src/main/java/com/modify/jabber/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.analytics.FirebaseAnalytics;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
